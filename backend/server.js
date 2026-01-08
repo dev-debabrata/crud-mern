2065a02f058242aa0a17978405dd8086997e41b4
Debabrata Das <debabratadas711@gmail.com>
2026-01-08 18:45:21 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,9 @@ import employeeRoutes from "./routes/employee.routes.js";
 
 dotenv.config();
 
-
 const app = express();
+
+connectDB();
 
 app.use(
     cors({
@@ -19,9 +20,11 @@ app.use(express.json());
 
 app.use("/api/employees", employeeRoutes);
 
-const PORT = process.env.PORT || 5000;
+export default app;
 
-app.listen(PORT, () => {
-    connectDB();
-    console.log("Server is running on port: ", PORT);
-});
+const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log("Server is running on port: ", PORT);
+    });
+}
